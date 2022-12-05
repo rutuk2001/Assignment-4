0a8fc2eb8701035f2c1671c8c8513e1d8fb70cfe
Rutuja Kharche <rutuja.kharche@gmail.com>
2022-12-05 10:11:48 +0530

    ejs

---
+++ app.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const PORT=2424;;
+const app=express();
+
+//setting app engine
+
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+// db connection
+
+const connectionString="mongodb://127.0.0.1:27017/mongocrudassn";
+mongoose
+  .connect(connectionString)
+  .then(res=> console.log("Database connected"))
+  .catch(err=> console.log("Error : "+err))
+//db end 
+const mainRoutes=require('./index');
+app.use("/",mainRoutes);
+app.listen(PORT,(err)=>{
+    if(err) throw err;
+    else console.log(`Work On ${PORT}`)
+})
